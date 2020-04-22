78b324011c757dade048b30a67087ff04de7b02b
Aljamil1113 <aarazasaljamil@gmail.com>
2020-04-22 23:36:24 +0800

    Categories,Reviewers Create Update Delete

--- BookGUI/Controllers/CategoriesController.cs
+++ BookGUI/Controllers/CategoriesController.cs
@@ -1,8 +1,10 @@
+using System.Net.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BookApi.Dtos;
+using BookApi.Model;
 using BookGUI.Services;
 using BookGUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -58,6 +60,134 @@ namespace BookGUI.Controllers
             };
 
             return View(categoryViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category addCategory)
+        {
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5000/api/");
+                var responseTask = client.PostAsJsonAsync("categories", addCategory);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    var newCategoryTask = result.Content.ReadAsAsync<Category>();
+                    newCategoryTask.Wait();
+
+                    var newCategory = newCategoryTask.Result;
+                    TempData["SuccessMessage"] = $"Category {newCategory.Name} was successfully created.";
+                
+                    return RedirectToAction("Detail", new { categoryId = newCategory.Id });
+                }
+
+                if((int)result.StatusCode == 422)
+                {
+                    ModelState.AddModelError("", "Category already exists.");
+                }
+
+                else
+                {
+                    ModelState.AddModelError("", "Some kind of error. Category not created!");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int categoryId)
+        {
+            var categoryToUpdate = categoriesRepository.GetCategory(categoryId);
+
+            if(categoryToUpdate == null)
+            {
+                ModelState.AddModelError("", "Error getting a category");
+                categoryToUpdate = new CategoryDto();
+            }
+            return View(categoryToUpdate);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category categoryToUpdate)
+        {
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5000/api/");
+                var responseTask = client.PutAsJsonAsync($"categories/{categoryToUpdate.Id}", categoryToUpdate);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    TempData["SuccessMessage"] = $"Category was successfully updated.";
+
+                    return RedirectToAction("Detail", new { categoryId = categoryToUpdate.Id});
+                }
+
+                if((int)result.StatusCode == 422)
+                {
+                    ModelState.AddModelError("", "Category already exists.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Some kind of error. Category not created!");
+                }
+            }
+            var categoryDto = categoriesRepository.GetCategory(categoryToUpdate.Id);
+            return View(categoryDto);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int categoryId)
+        {
+            var categoryToDelete = categoriesRepository.GetCategory(categoryId);
+
+            if(categoryToDelete == null)
+            {
+                ModelState.AddModelError("", "Error getting a category");
+                categoryToDelete = new CategoryDto();
+            }
+
+            return View(categoryToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int categoryId, string categoryName)
+        {
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5000/api/");
+                var responseTask = client.DeleteAsync($"categories/{categoryId}");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    TempData["SuccessMessage"] = $"Category {categoryName} was successfully deleted.";
+
+                    return RedirectToAction("Index","Categories");
+                }
+
+                if((int)result.StatusCode == 409)
+                {
+                    ModelState.AddModelError("", $"Category {categoryName} cannot be deleted because " +
+                    $"it is used by at least one book.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Some kind of error. Category not created!");
+                }
+            }
+            var categoryDto = categoriesRepository.GetCategory(categoryId);
+            return View(categoryDto);
         }
     }
 }
--- BookGUI/Controllers/ReviewersController.cs
+++ BookGUI/Controllers/ReviewersController.cs
@@ -1,8 +1,10 @@
+using System.Net.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BookApi.Dtos;
+using BookApi.Model;
 using BookGUI.Services;
 using BookGUI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -69,6 +71,122 @@ namespace BookGUI.Controllers
              };
 
              return View(reviewerReviewsBookModel);
+         }
+
+         [HttpGet]
+         public IActionResult CreateReviewer()
+         {
+             return View();
+         }
+
+         [HttpPost]
+         public IActionResult CreateReviewer(Reviewer addReviewer)
+         {
+             using (var client = new HttpClient())
+             {
+                 client.BaseAddress = new Uri("http://localhost:5000/api/");
+                 var responseTask = client.PostAsJsonAsync("reviewers", addReviewer);
+                 responseTask.Wait();
+
+                 var result = responseTask.Result;
+
+                 if(result.IsSuccessStatusCode)
+                 {
+                     var newReviewerTask = result.Content.ReadAsAsync<Reviewer>();
+                     newReviewerTask.Wait();
+
+                     var newReviewer = newReviewerTask.Result;
+
+                     TempData["SuccessMessage"] = $"Reviewer {newReviewer.LastName}, {newReviewer.FirstName} successfully created!";
+                     return RedirectToAction("DetailReviewer", new {reviewerId = newReviewer.Id});
+                 }
+
+                 if((int) result.StatusCode == 500)
+                 {
+                     ModelState.AddModelError("", "Some kind of error. Reviewer not created!");
+                 }
+             }
+             return View();
+         }
+
+         [HttpGet]
+         public IActionResult Edit(int reviewerId)
+         {
+             var reviewerToUpdate = reviewerRepository.GetReviewer(reviewerId);
+
+             if(reviewerToUpdate == null)
+             {
+                 ModelState.AddModelError("", "Error getting the reviewer");
+                 reviewerToUpdate = new ReviewerDto();
+             }
+
+             return View(reviewerToUpdate);
+         }
+
+         [HttpPost]
+         public IActionResult Edit(Reviewer reviewerToUpdate)
+         {
+             using (var client = new HttpClient())
+             {
+                 client.BaseAddress = new Uri("http://localhost:5000/api/");
+                 var responseTask = client.PutAsJsonAsync($"reviewers/{reviewerToUpdate.Id}", reviewerToUpdate);
+                 responseTask.Wait();
+
+                 var result = responseTask.Result;
+                 if(result.IsSuccessStatusCode)
+                 {
+                     ViewData["SuccessMessage"] = $"Successfully update Reviewer";
+                     return RedirectToAction("DetailReviewer", new{reviewerId = reviewerToUpdate.Id});
+                 } 
+
+                 if((int)result.StatusCode == 500)
+                 {
+                     ModelState.AddModelError("", "Some error updating the reviewer!");
+                 }
+             }
+             var reviewerDto = reviewerRepository.GetReviewer(reviewerToUpdate.Id);
+             return View(reviewerDto);
+         }
+
+         [HttpGet]
+         public IActionResult Delete(int reviewerId)
+         {
+             var reviewer = reviewerRepository.GetReviewer(reviewerId);
+
+             if(reviewer == null)
+             {
+                 ModelState.AddModelError("", "Error getting reviewer!");
+                 reviewer = new ReviewerDto();
+             }
+
+             return View(reviewer);
+         }
+
+         [HttpPost]
+         public IActionResult Delete(int reviewerId, string firstName, string lastName)
+         {
+             using (var client = new HttpClient())
+             {
+                 client.BaseAddress = new Uri("http://localhost:5000/api/");
+                 var responseTask = client.DeleteAsync($"reviewers/{reviewerId}");
+                 responseTask.Wait();
+
+                 var result = responseTask.Result;
+                 if(result.IsSuccessStatusCode)
+                 {
+                     TempData["SuccessMessage"] = $"Successfully delete reviewer {lastName}, {firstName}";
+                     return RedirectToAction("Index","Reviewers");
+                 }
+
+                 if((int)result.StatusCode == 500)
+                 {
+                     ModelState.AddModelError("","Some kind of error, reviewer not deleted!");
+                 }
+
+                 var reviewerDto = reviewerRepository.GetReviewer(reviewerId);
+                return View(reviewerDto);
+             }
+            
          }
         
     }
